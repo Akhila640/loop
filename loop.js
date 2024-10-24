2757242cde59076f3591bdc697cb645fe56f82c7
Akhila640 <akhilasiddi00@gmail.com>
2024-10-24 10:17:31 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,53 @@
+// Example of For loop
+for(var a=1;a<=5;a++){
+    console.log(a)
+}
+
+for(var a=5;a>=1;a--){
+    console.log(a)
+}
+
+
+for(var i=0;i<=5;i++){
+}
+console.log(i)
+
+// Example of While loop
+var a=1
+while(a<=7){
+    console.log(a)
+    a++
+}
+
+
+var a=10
+while(a>=5){
+    console.log(a)
+    a--
+}
+
+
+var i=1
+while(i<4){
+    i++
+}
+console.log(i)
+
+// Example of DO-While loop
+var a=1;
+do{
+    a++
+    console.log(a)
+}while(a<=5)
+
+var a=10;
+    do{
+        a--
+        console.log(a)
+    }while(a>=7)
+
+var a=10;
+do{
+    console.log(a)
+    a--
+}while(a>7)
